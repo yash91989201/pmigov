b4b893432d4d1f4a048fa24dc09634ee1154cf0d
Yashraj Jaiswal <yashraj6370@gmail.com>
2026-06-17 18:43:15 +0530

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import { createDb } from "@pmigov/db";
+import * as schema from "@pmigov/db/schema/auth";
+import { env } from "@pmigov/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [tanstackStartCookies()],
+  });
+}
+
+export const auth = createAuth();
